f451eff9209a90e5dfbd7b673b1630227bc7e77a
Alberto Gonzalez <81926665+albertogonzalezm@users.noreply.github.com>
2023-02-14 11:41:20 -0500

    Update settings.py

--- django_password_generator/settings.py
+++ django_password_generator/settings.py
@@ -19,7 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%mv-=re9#dy$u@-wti+y+&x2b&23qs5%4+xqlq864$+k@0vs0m'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+if SECRET_KEY is None:
+  print("Please setup a SECRET_KEY in the Secrets (Environment variables) tab.") # secrets.token_urlsafe(32)
+  exit(1)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
