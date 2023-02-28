b5ba447e30f6706cb09c088592a18582c69a1bfe
Alberto Gonzalez <81926665+albertogonzalezm@users.noreply.github.com>
2023-02-28 10:44:53 -0500

    last update

--- django_password_generator/settings.py
+++ django_password_generator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+
+SECRET_KEY = os.getenv('SECRET_KEY') # secrets.token_urlsafe(32)
 
 if SECRET_KEY is None:
-  print("Please setup a SECRET_KEY in the Secrets (Environment variables) tab.") # secrets.token_urlsafe(32)
+  print("Please setup a SECRET_KEY in the Secrets (Environment variables) tab.")
   exit(1)
 
 # SECURITY WARNING: don't run with debug turned on in production!
